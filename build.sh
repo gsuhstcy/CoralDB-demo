f1d31ffd1ef78d59698e257bf5e84430928cd1f6
gsuhstcy <772473590@qq.com>
2025-12-18 15:25:50 +0800

    Auto create superuser on Render

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# 自动创建管理员（如果已存在会跳过）
+python manage.py createsuperuser --noinput || true
