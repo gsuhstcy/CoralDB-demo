a3f308d80b9614cbbc975240530f51eaef4650a8
gsuhstcy <772473590@qq.com>
2025-12-19 10:58:54 +0800

    Serve static files with WhiteNoise

--- coraldb_site/settings.py
+++ coraldb_site/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = "coraldb_site.urls"
 
@@ -113,6 +116,7 @@ USE_TZ = True
 # --------------------------------------------------
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # ⚠️ 如果你没有 static 目录，这里不要写 STATICFILES_DIRS
 # STATICFILES_DIRS = [BASE_DIR / "static"]
